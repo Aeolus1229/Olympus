9c892a9ae507b8d78b83b83d50bae1a1824f4172
Aeolus1229 <78156103+Aeolus1229@users.noreply.github.com>
2021-12-01 14:00:39 -0600

    Update LegacyMod.js

--- LegacyMod.js
+++ LegacyMod.js
@@ -5,7 +5,7 @@ desc:'A simple mod for reaching bounds no man ever has.',
 engineVersion:1,
 manifest:'modManifest.js',
 requires:['Default dataset*'],
-sheets:{'spicySheet':'img/spicyModIconSheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+sheets:{'spicySheet':'https://github.com/Aeolus1229/Olympus/blob/JSProjects/vampModIconSheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
 func:function()
 {
 	//The idea in this simple example mod is to add a few elements focused around hot sauce, because hot sauce is great and I use that stuff everywhere.
